bf5fe83adaacae12c63985e77e492761b3ff4c6e
nctrungggg <nctrung04102000@gmail.com>
2020-11-28 18:59:18 +0700

    first commit

---
+++ Buổi 1 js/index.js
@@ -0,0 +1,33 @@
+//biến: let, var
+
+let hello = "Hùng yêu Vân Anh";
+
+//Hằng: const
+const USERNAME = "Black"
+
+//Mảng
+let arrStudent = ['a', 'b', 'c']
+console.log(arrStudent);
+
+//nối chuỗi, hiện thị độ dài mảng: lenght
+console.log('Length:' +  ' '  + arrStudent.length);
+
+//lấy phẩn tử trong mảng: [chỉ mục]
+console.log(arrStudent[0]);
+
+//Function
+function ten () {
+    alert('NCT')
+}
+ten()
+
+//Arrow function: Tên = (truyền biến) => {}
+//Không cần contructor, không cần this
+arrHeloo = () => {
+    alert("ok")
+
+}
+arrHeloo()
+//This: là con trỏ, chỉ vào đối tượng nào đấy, chỉ function, const
+
+
---
+++ Buổi 1 js/script.js
@@ -0,0 +1,14 @@
+hello = () => {
+    let a = 100 + 200
+    console.log(a);
+}
+hello()
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+console.log(arr[7]);
+console.log(arr);
+console.log(arr.splice(5, 1));
+
+let c = arr.map(num => num * 3 )
+console.log(c);
